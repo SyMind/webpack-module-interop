fc30fb077dbbf1a2aed3e7f70c8590198e5f700c
Cong-Cong <dacongsama@live.com>
2024-03-05 09:43:39 +0800

    import cjs module

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import { add } from 'esm';
+import * as esm from 'esm';
+import { subtraction } from 'cjs';
+import * as cjs from 'cjs';
 
-const result = add(1, 2);
-console.log('result: ', result);
+console.log('1 + 2: ', add(1, 2));
+console.log('1 + 2: ', esm.add(1, 2));
+console.log('2 - 1: ', subtraction(2, 1));
+console.log('2 - 1: ', cjs.subtraction(2, 1));
